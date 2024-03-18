6f400f990d5083bd03ea9a544d795dc42f5c4f90
gabrieldasilva <josedasilva1999@outlook.es>
2024-03-18 15:46:27 -0300

    correcciones

--- src/dao/models/carts.models.js
+++ src/dao/models/carts.models.js
@@ -2,23 +2,27 @@ import mongoose from "mongoose";
 
 const cartsCollection = 'carts';
 
-const productInCartSchema = new mongoose.Schema({
-    product: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "products"
-    },
-    title: String,
-    description: String,
-    price: Number,
-    quantity: {
-        type: Number,
-        default: 0
+const cartsSchema = new mongoose.Schema({
+    products: {
+        type: [
+            {
+                product: {
+                    type: mongoose.Schema.Types.ObjectId,
+                    ref: "products"
+                },
+                title: String,
+                description: String,
+                price: Number,
+                quantity: {
+                    type: Number,
+                    default: 0
+                }
+            }
+        ]
     }
 });
 
-const cartsSchema = new mongoose.Schema({
-    products: [productInCartSchema] // Utiliza el esquema definido para los productos en el carrito
-});
+
 
 
 
